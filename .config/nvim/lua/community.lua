7d445d138ad9e9770496fda8edb3bdd5a4fb0dcc
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-08-25 10:21:56 +0200

    cleanup

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -9,6 +9,7 @@ return {
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.mini-ai" },
