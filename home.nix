9fc4376c43094f2a73524c0824f229d0185240b5
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-22 23:19:02 +0100

    Enable and configure git

--- home.nix
+++ home.nix
@@ -18,4 +18,19 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Ryan Malonzo";
+    userEmail = "64932650+ryanmalonzo@users.noreply.github.com";
+    signing = {
+      key = "/Users/ren/.ssh/git.pub";
+      signByDefault = true;
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      gpg.format = "ssh";
+      pull.rebase = true;
+    };
+  };
 }
