76fbc6dd056211a6b1fe76e0fec49d8836fabbfb
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-23 22:27:22 +0100

    Install nerd font

--- home/common.nix
+++ home/common.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  fonts.fontconfig.enable = true;
+  
+  home.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "IBMPlexMono" ]; })
+  ];
+
   editorconfig = {
     enable = true;
     settings = {
