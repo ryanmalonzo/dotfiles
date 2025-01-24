2b2d0eaa300305c52110ea58cf8f5d590cd44c78
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-24 09:27:17 +0100

    Fix Firefox on darwin

--- flake.nix
+++ flake.nix
@@ -7,39 +7,41 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-firefox-darwin.url = "github:bandithedoge/nixpkgs-firefox-darwin";
+    nixpkgs-firefox-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    # Helper function to create Darwin configurations
-    mkDarwinSystem = { system ? "aarch64-darwin", hostname, username ? "ren" }: 
-      nix-darwin.lib.darwinSystem {
-        inherit system;
-        modules = [
-          ./hosts/common.nix
-          ./hosts/darwin-common.nix
-          (./hosts + "/${hostname}")
-          home-manager.darwinModules.home-manager
-          {
-            users.users.${username}.home = "/Users/${username}";
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import (./home + "/${username}");
-          }
-        ];
+  outputs = { self, nixpkgs, home-manager, nixpkgs-firefox-darwin, nix-darwin, ... }@inputs:
+    let
+      mkDarwinSystem = { system ? "aarch64-darwin", hostname, username ? "ren" }: 
+        nix-darwin.lib.darwinSystem {
+          inherit system;
+          modules = [
+            ./hosts/common.nix
+            ./hosts/darwin-common.nix
+            (./hosts + "/${hostname}")
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs.overlays = [ inputs.nixpkgs-firefox-darwin.overlay ];
+              users.users.${username}.home = "/Users/${username}";
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${username} = import (./home + "/${username}");
+            }
+          ];
+        };
+    in
+    {
+      darwinConfigurations = {
+        "Ryans-MacBook-Air" = mkDarwinSystem {
+          hostname = "Ryans-MacBook-Air";
+          username = "ren";
+        };
+        # Add more Darwin hosts here as needed:
+        # "Other-Mac" = mkDarwinSystem {
+        #   hostname = "Other-Mac";
+        #   username = "otheruser";
+        # };
       };
-  in
-  {
-    darwinConfigurations = {
-      "Ryans-MacBook-Air" = mkDarwinSystem {
-        hostname = "Ryans-MacBook-Air";
-        username = "ren";
-      };
-      # Add more Darwin hosts here as needed:
-      # "Other-Mac" = mkDarwinSystem {
-      #   hostname = "Other-Mac";
-      #   username = "otheruser";
-      # };
     };
-  };
 }
