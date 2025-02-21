60b9d0895adaf5f372586f520fc1d1b955c2131d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-02-21 17:48:49 +0100

    Add brew to PATH

--- home/darwin-common.nix
+++ home/darwin-common.nix
@@ -1,9 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  # Add brew to PATH
+  programs.zsh.initExtra = ''
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  '';
+
   # Rectangle
   home.file."Library/Application Support/Rectangle/RectangleConfig.json" = {
     source = ../config/rectangle/RectangleConfig.json;
     recursive = true;
   };
-} 
+}
