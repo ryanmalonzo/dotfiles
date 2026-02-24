3a6130ae5bd8bd89c17b905c7b99f5f663feefcc
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-24 23:19:43 +0100

    use smart-splits
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -18,7 +18,6 @@
           set -g @catppuccin_window_status_style "rounded"
         '';
       }
-      vim-tmux-navigator
     ];
 
     extraConfig = ''
@@ -28,6 +27,16 @@
       unbind %
 
       bind c new-window -c "#{pane_current_path}"
+
+      # smart-splits: navigation and resizing, aware of Neovim splits
+      bind -n C-h if -F "#{@pane-is-vim}" 'send-keys C-h' 'select-pane -L'
+      bind -n C-j if -F "#{@pane-is-vim}" 'send-keys C-j' 'select-pane -D'
+      bind -n C-k if -F "#{@pane-is-vim}" 'send-keys C-k' 'select-pane -U'
+      bind -n C-l if -F "#{@pane-is-vim}" 'send-keys C-l' 'select-pane -R'
+      bind -n M-h if -F "#{@pane-is-vim}" 'send-keys M-h' 'resize-pane -L 3'
+      bind -n M-j if -F "#{@pane-is-vim}" 'send-keys M-j' 'resize-pane -D 3'
+      bind -n M-k if -F "#{@pane-is-vim}" 'send-keys M-k' 'resize-pane -U 3'
+      bind -n M-l if -F "#{@pane-is-vim}" 'send-keys M-l' 'resize-pane -R 3'
 
       set -g mouse on
 
