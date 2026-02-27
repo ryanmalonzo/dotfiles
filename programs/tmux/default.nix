d678b69704a27a7a0ab15bfa0b77d6b751155c8b
Ryan Malonzo <ryan.malonzo@spendesk.com>
2026-02-27 10:40:16 +0100

    fix shift+enter to go to newline with tmux

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -39,6 +39,10 @@
       bind -n M-k if -F "#{@pane-is-vim}" 'send-keys M-k' 'resize-pane -U 3'
       bind -n M-l if -F "#{@pane-is-vim}" 'send-keys M-l' 'resize-pane -R 3'
 
+      set -g extended-keys on
+      set -ga terminal-features '*:extkeys'
+      bind -n S-Enter send-keys -H 1b 5b 31 33 3b 32 75
+
       set -g mouse on
 
       # Start windows and panes at 1
