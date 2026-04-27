50fe30ec8e2db3d6d963fdb06fe32e0fd9b098b8
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-04-27 22:59:06 +0200

    update aliases
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- programs/shell/aliases.nix
+++ programs/shell/aliases.nix
@@ -1,15 +1,14 @@
 {
   ccopy = "pbcopy < ";
-  cls = "clear";
   cpaste = "pbpaste";
   dcd = "docker compose down";
   dce = "docker compose exec";
   dcu = "docker compose up -d";
   dcp = "docker compose ps";
   gp = "git pull";
+  gps = "git pull && git switch";
   gs = "git switch";
   lg = "lazygit";
-  v = "nvim";
   vi = "nvim";
   vim = "nvim";
   vx = "nvim .";
