a477fb2213572c083e292e0723de53ba0799cb2a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-05-22 22:03:57 +0200

    feat(home): base config

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,6 +1,76 @@
-{ username, homeDirectory, ... }:
+{
+  lib,
+  pkgs,
+  username,
+  homeDirectory,
+  ...
+}:
 {
   home.username = username;
   home.homeDirectory = homeDirectory;
   home.stateVersion = "25.11";
+
+  programs.home-manager.enable = true;
+
+  fonts.fontconfig.enable = true;
+
+  home.sessionPath = [
+    "$HOME/.npm-global/bin"
+    "$HOME/go/bin"
+  ];
+
+  home.sessionVariables = {
+    DO_NOT_TRACK = "true";
+    EDITOR = "nvim";
+    GH_TELEMETRY = "false";
+    LANG = "en_US.UTF-8";
+    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    NPM_CONFIG_PREFIX = "$HOME/.npm-global";
+  };
+
+  home.packages = with pkgs; [
+    bat
+    cargo
+    fnm
+    csview
+    deadnix
+    delta
+    eza
+    fd
+    fzf
+    gh
+    glow
+    go
+    jless
+    lazygit
+    nixd
+    nixfmt-rfc-style
+    ripgrep
+    ruff
+    statix
+    uv
+  ];
+
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        indent_size = 2;
+        indent_style = "space";
+        end_of_line = "lf";
+        insert_final_newline = true;
+        trim_trailing_whitespace = true;
+      };
+      "*.{md,mdx,diff}".trim_trailing_whitespace = false;
+      "*.py".indent_size = 4;
+      "*.{cmd,bat,ps1}".end_of_line = "crlf";
+    };
+  };
+
+  programs.zsh.initContent = lib.mkBefore ''
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  '';
+
+  imports = [ ./programs/shell.nix ];
 }
---
+++ modules/home/programs/shell.nix
@@ -0,0 +1,66 @@
+{ ... }:
+{
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+
+    history = {
+      size = 50000;
+      save = 50000;
+      ignoreDups = true;
+      ignoreSpace = true;
+      share = true;
+      extended = true;
+    };
+
+    autosuggestion.strategy = [
+      "match_prev_cmd"
+      "history"
+      "completion"
+    ];
+    historySubstringSearch.enable = true;
+
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -la";
+      lt = "eza --tree";
+      cat = "bat";
+      cd = "z";
+      ci = "zi";
+      ccopy = "pbcopy < ";
+      cpaste = "pbpaste";
+      dcd = "docker compose down";
+      dce = "docker compose exec";
+      dcu = "docker compose up -d";
+      dcp = "docker compose ps";
+      gp = "git pull";
+      gps = "git pull && git switch";
+      gs = "git switch";
+      lg = "lazygit";
+      vi = "nvim";
+      vim = "nvim";
+      vx = "nvim .";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      direnv.disabled = false;
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.zoxide.enable = true;
+
+  programs.zsh.initContent = ''
+    eval "$(fnm env --use-on-cd --shell zsh)"
+  '';
+}
