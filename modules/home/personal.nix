34316b43bd25b4024eff357072aa62e59ecab539
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-07-19 18:12:47 +0200

    feat(personal): add homelab to ssh config
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- modules/home/personal.nix
+++ modules/home/personal.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 {
+  programs.ssh.matchBlocks."chaldea" = {
+    hostname = "192.168.1.35";
+    user = "ren";
+    port = 2222;
+    identityFile = "~/.ssh/chaldea";
+  };
+
   home.packages = with pkgs; [
     biome
     bun
