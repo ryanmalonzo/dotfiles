f8ca0dc35dc14dc4b683defd4da9879a29dbe967
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-04 19:24:48 +0200

    chore(work): uninstall cloudflared
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- profiles/work.nix
+++ profiles/work.nix
@@ -9,7 +9,6 @@
   gitSigningKeyPub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA3VolUY+dMMyQoCZQS2XhWAgyEH9EFhiDsbWpH6IAod";
 
   extraBrews = [
-    "cloudflared" # nixpkgs package doesn't build on 26.05
     "datadog-labs/pack/pup"
   ];
   extraCasks = [
