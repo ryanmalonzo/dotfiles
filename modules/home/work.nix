91701b4be03af711c5e0bd127a5d80e2fca73ef7
Ryan Malonzo <ryan.malonzo@spendesk.com>
2026-09-16 11:25:55 +0200

    feat(work): set HOMEBREW_GITHUB_API_TOKEN env var
    Signed-off-by: Ryan Malonzo <ryan.malonzo@spendesk.com>

--- modules/home/work.nix
+++ modules/home/work.nix
@@ -31,6 +31,7 @@
 
     export GH_TOKEN=$(cat ${config.sops.secrets.gh_token.path})
     export GITHUB_TOKEN=$(cat ${config.sops.secrets.github_token.path})
+    export HOMEBREW_GITHUB_API_TOKEN=$(cat ${config.sops.secrets.github_token.path})
     export LOKALISE_API_TOKEN=$(cat ${config.sops.secrets.lokalise_api_token.path})
     export NPM_TOKEN=$(cat ${config.sops.secrets.npm_token.path})
 
